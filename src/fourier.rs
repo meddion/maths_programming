3dc5bf453f9997dc878701d4406d648be1efdf23
meddion <volodia23byk@gmail.com>
2020-05-12 16:29:54 +0300

    add more meaningfull comments

--- src/fourier.rs
+++ src/fourier.rs
@@ -1,3 +1,9 @@
+/// This module contains two famous equations, parts of the fourier transform,
+/// implemented in a straightforward manner.
+/// I'll be honest, this whole fourier thing still pretty much confuses me;
+/// gotta take a closer look someday on it.
+/// Sources: [https://betterexplained.com/articles/an-interactive-guide-to-the-fourier-transform/]
+
 pub const TAU: f32 = 6.283_185_5;
 pub struct Complex(f32, f32);
 
@@ -7,10 +13,10 @@ pub fn transform(data: &[f32]) -> Vec<Complex> {
     for freq in 0..n {
         let mut comp = Complex(0.0, 0.0);
         let rate = TAU * freq as f32 / n as f32;
-        for t in 0..n {
+        for (t, amplitude) in data.iter().enumerate() {
             let distance = rate * t as f32;
-            comp.0 += data[t] * distance.cos();
-            comp.1 += data[t] * distance.sin();
+            comp.0 += amplitude * distance.cos();
+            comp.1 += amplitude * distance.sin();
         }
         if comp.0.abs() < 1E-6 {
             comp.0 = 0.0;
@@ -27,15 +33,15 @@ pub fn inverse_transform(data: &[f32]) -> Vec<Complex> {
     let n = data.len();
     let mut result = Vec::with_capacity(n);
     for freq in 0..n {
-        // τ is there, since as t goes from 0 to 1
-        // it needs to cover a distance of τ along the circle (circumferences);
-        // in conjunction with freq it describes how much distance is covered in 1 sec.
+        // τ is there, since as t goes from 0 to 1, and
+        // it needs to cover a distance of τ along the circle (circumference).
+        // In conjunction with freq, the expression describes how much distance is covered in 1 sec.
         let mut comp = Complex(0.0, 0.0);
         let rate = -TAU * freq as f32 / n as f32;
-        for t in 0..n {
+        for (t, amplitude) in data.iter().enumerate() {
             let distance = rate * t as f32;
-            comp.0 += data[t] * distance.cos();
-            comp.1 += data[t] * distance.sin();
+            comp.0 += amplitude * distance.cos();
+            comp.1 += amplitude * distance.sin();
         }
         if comp.0.abs() < 1E-6 {
             comp.0 = 0.0;
@@ -56,7 +62,8 @@ mod test {
     use super::*;
 
     #[test]
-    fn fourier_transform_test() {
+    // Not really a unit test, but more like a showcase.
+    fn fourier_transform_showcase() {
         let data_points = &[8.0, 6.0, 7.0, 11.0, 2.0, 0.0, 1.0, 8.0, 3.0];
         let output = transform(data_points);
         println!("Transform:");
